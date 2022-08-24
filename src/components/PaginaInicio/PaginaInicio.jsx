655bd7ac4b967453f9d17d6bb00fbd3729060d9d
Tomas Levy <tomaslevy1999@gmail.com>
2022-08-24 00:28:00 -0300

    push

--- src/components/PaginaInicio/PaginaInicio.jsx
+++ src/components/PaginaInicio/PaginaInicio.jsx
@@ -6,14 +6,14 @@ import logo from '../../assets/logo3.jpeg';
 
 
 function PaginaInicio(props){
-    const [isDown,setIsDown]= useState(true);
-    const [dButtonIsShowed, setDButtonIsShowed]=useState(false)
+    const [textIsShowed,setTextIsShowed]= useState(false);
+    const [buttonIsShowed, setButtonIsShowed]=useState(false)
 
     function mostrarTexto(){
-        setIsDown(false);
+        setTextIsShowed(true);
     }
     function sacarTexto(){
-        setIsDown(true);
+        setTextIsShowed(false);
     }
 
     useEffect(()=>{
@@ -32,14 +32,14 @@ function PaginaInicio(props){
           );
           mostrarTexto();
           setTimeout(()=>{
-            document.querySelector("picture").setAttribute('style','transform:translateY(-600px);background:white');
-            setDButtonIsShowed(true);
+            document.querySelector("picture").setAttribute('style','transform:translateY(-600px)');
+            setButtonIsShowed(true);
           },850);
                
 
         };
         function desAnimate(){
-            setDButtonIsShowed(false);
+            setButtonIsShowed(false);
             document.querySelector("picture").animate([
                 { transform: 'translateY(0px)' },
               ], {
@@ -58,19 +58,20 @@ function PaginaInicio(props){
         <div className='contenedor-inicio'>
             <picture className='imagen-inicio'>
                 <img src={logo} alt=""/></picture>
-            <div className='contenedor-texto-inicio'>
-                {isDown ? null:
-                <div className='texto-inicio'>
-                    <h1 className='texto-inicio-titulo'>SOBRE NOSOTROS</h1>
-                    <p className='texto-inicio-parrafo'>
-                        <b>¡Hola!</b><br/> 
-                        Bienvenidos a <b>Kob et Lys</b> y gracias por acompañarnos!<br/>
-                        Somos un emprendimiento familiar y queremos brindarte todos los productos para darle un estilo único y personal a tu hogar.<br/>
-                        Sabemos que la calidad de vida que te mereces ¡es posible! y por eso hacemos que tu casa trabaje con vos, contamos con productos inteligentes para hacer tu día a día y el de tus afectos más fácil y seguro.<br/>
-                        ¡Animate a vivir la experiencia!</p>
-                </div> }
-            </div>
-            {isDown?
+                {!textIsShowed ? null:
+                <div className='contenedor-texto-inicio'>
+                    <div className='texto-inicio'>
+                        <h1 className='texto-inicio-titulo'>SOBRE NOSOTROS</h1>
+                        <p className='texto-inicio-parrafo'>
+                            <b>¡Hola!</b><br/> 
+                            Bienvenidos a <b>Kob et Lys</b> y gracias por acompañarnos!<br/>
+                            Somos un emprendimiento familiar y queremos brindarte todos los productos para darle un estilo único y personal a tu hogar.<br/>
+                            Sabemos que la calidad de vida que te mereces ¡es posible! y por eso hacemos que tu casa trabaje con vos, contamos con productos inteligentes para hacer tu día a día y el de tus afectos más fácil y seguro.<br/>
+                            ¡Animate a vivir la experiencia!</p>
+                    </div> 
+                </div>}
+            
+            {!buttonIsShowed ?
             <div className='container-down-buton'>
                 <button className='down-buton' onClick={()=>{
                     animate();
@@ -78,7 +79,7 @@ function PaginaInicio(props){
                     }}><span className="fa fa-angle-right"></span>
                 </button>
             </div> :null}
-            {dButtonIsShowed ?  
+            {buttonIsShowed ?  
             <div className='container-up-buton'>
                 <button className='up-buton' onClick={()=>{
                     desAnimate();
