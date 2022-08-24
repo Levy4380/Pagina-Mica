e2151f54e8cc697a0b6f1b50b70727d2256a5555
Tomas Levy <tomaslevy1999@gmail.com>
2022-08-23 23:11:46 -0300

    push

--- src/components/PaginaInicio/PaginaInicio.jsx
+++ src/components/PaginaInicio/PaginaInicio.jsx
@@ -34,7 +34,7 @@ function PaginaInicio(props){
           setTimeout(()=>{
             document.querySelector("picture").setAttribute('style','transform:translateY(-600px);background:white');
             setDButtonIsShowed(true);
-          },899);
+          },850);
                
 
         };
@@ -62,8 +62,9 @@ function PaginaInicio(props){
                 {isDown ? null:
                 <div className='texto-inicio'>
                     <h1 className='texto-inicio-titulo'>SOBRE NOSOTROS</h1>
-                    <p className='texto-inicio-parrafo'>¡Hola!<br/>
-                        Bienvenidos a Kob et Lys y gracias por acompañarnos!<br/>
+                    <p className='texto-inicio-parrafo'>
+                        <b>¡Hola!</b><br/> 
+                        Bienvenidos a <b>Kob et Lys</b> y gracias por acompañarnos!<br/>
                         Somos un emprendimiento familiar y queremos brindarte todos los productos para darle un estilo único y personal a tu hogar.<br/>
                         Sabemos que la calidad de vida que te mereces ¡es posible! y por eso hacemos que tu casa trabaje con vos, contamos con productos inteligentes para hacer tu día a día y el de tus afectos más fácil y seguro.<br/>
                         ¡Animate a vivir la experiencia!</p>
